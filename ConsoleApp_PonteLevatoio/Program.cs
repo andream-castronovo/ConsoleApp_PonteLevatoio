0d15bac9a9849af8405aa53fc653113b6a8a56c4
andream-castronovo <andreamaria.castronovo.stud@ispascalcomandini.it>
2023-02-18 11:13:40 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleApp_PonteLevatoio/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_PonteLevatoio
+{
+    internal class Program
+    {
+
+        /*
+         Traccia a pagina 163
+            
+          Prima versione semplificata:
+           - Le barche hanno sempre la precedenza
+           - Senso unico
+
+           - 4 bottoni: 
+            - A (Spawna una macchina) 
+            - O (Apre il ponte)
+            - C (Chiude il ponte)
+            - U (Chiude il programma)
+
+          Quando premo A una macchina spunta nello schermo e deve passare attraverso il ponte.
+          Se viene richiesta l'apertura del ponte, le macchine che sono sopra finiscono di transitare, le altre non passano e aspettano l'apertura del ponte.
+        
+          Usare Semaphore SOLO per limitare le auto sul ponte!
+         */
+
+
+
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        
+    }
+}
